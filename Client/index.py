e4fdbf23959672a88ab47b58a57cce77d752b06d
Callum Cheng <callum.cheng@capgemini.com>
2024-10-29 21:29:26 +0000

    client sends reading to server

--- Client/index.py
+++ Client/index.py
@@ -1,6 +1,21 @@
 import socketio
+import json
+from datetime import datetime
 
 socket = socketio.Client()
+
+def convert_to_json(id, time, usage):
+    # Create a dictionary
+    data = {
+        "id": id,
+        "time": time,
+        "usage": usage
+    }
+    # Convert the dictionary to a JSON string
+    json_object = json.dumps(data, indent=4)
+    # Convert the json string to JSON object
+    json_object = json.loads(json_object)
+    return json_object
 
 @socket.event
 def connect():
@@ -18,6 +33,12 @@ def disconnect():
 @socket.event
 def Hello(data):
     print("Hello message received: " + data)
+    id = 123
+    time = datetime.now().isoformat()
+    usage = 45.67
+    json_result = convert_to_json(id, time, usage)
+    print(json_result)
+    socket.emit('Hello World!', json_result)
 
 socket.connect("http://localhost:3000")
 socket.wait()
