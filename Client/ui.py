0eab423dc3eb4c5d9b5bdca428ae9ac032103239
Callum Cheng <callum.cheng@capgemini.com>
2024-10-29 01:27:02 +0000

    created gauge component

--- Client/ui.py
+++ Client/ui.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from gauge import GaugeFrame
+
 font_reg = ("Verdana", 14, "normal")
 font_small = ("Verdana", 10, "normal")
 font_bold = ("Verdana", 16, "bold")
@@ -25,6 +27,10 @@ lblBill = tk.Label(root, text="Bill", bg='black', fg='white', font=font_bold)
 lblBill.grid(row=1, column=1)
 lblBill = tk.Label(root, text="£10.11", bg='black', fg='white', font=font_reg)
 lblBill.grid(row=2, column=1)
+
+# Create and place the GaugeFrame
+gauge_frame = GaugeFrame(root)
+gauge_frame.grid(row=3, column=0)
 
 # Configure grid to expand
 root.grid_columnconfigure(0, weight=1)
